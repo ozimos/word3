2856024b6b2c124f58c45da705d6c81246668941
Tovieye Ozi <tovieye.ozi@gmail.com>
2017-11-21 11:03:17 +0100

    Fix file references

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,15 @@
-const myApp = require('../src/words2');
+const myApp = require('../src/words');
 
 
 const assert = require('chai').assert;
 
 describe('words', () => {
-   it('returns an object', function () {
-     assert.deepEqual(myApp.words('olly olly in come free'), {
-       olly: 2,
-       in: 1,
-       come: 1,
-       free: 1
-     });
-   });
- });
+  it('returns an object', () => {
+    assert.deepEqual(myApp.words('olly olly in come free'), {
+      olly: 2,
+      in: 1,
+      come: 1,
+      free: 1
+    });
+  });
+});
